cfa8d19314806b8455d38ef0ad24ef72e2bef992
franc <francesco.anastasio93@gmail.com>
2024-03-18 23:39:17 +0100

    First deploy

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse("templates/index.html", {"request": request})
 
 
 @app.get("/hello/{name}")
