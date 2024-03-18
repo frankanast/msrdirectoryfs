8b56c4b31ada22e87bf7dfd4962e37bf9c3bd33c
franc <francesco.anastasio93@gmail.com>
2024-03-18 23:25:31 +0100

    First deploy

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
-
 from json import JSONDecoder
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/", response_class=HTMLResponse)
-async def read_root():
-    html = ""
-    with open('index.html') as file:
-        html = file.read()
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
-    return html
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
