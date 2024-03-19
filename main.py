390baec5be7876481a4ec1da8f8b31fcc1d9b544
franc <francesco.anastasio93@gmail.com>
2024-03-19 18:23:43 +0100

    Restored to real use case.

--- main.py
+++ main.py
@@ -21,20 +21,11 @@ async def hworld(name: str):
 async def get_autocomplete_data():
     try:
         connection = psycopg2.connect(DATABASE_URL, sslmode='require')
-        # connection = psycopg2.connect(
-        #     database="dfg7578eodbk83",
-        #     host="cdgn4ufq38ipd0.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com",
-        #     user="ub72dcmj38e4l2",
-        #     password="p3d3b7bc9f84595d79b5e170cef0107c5cc78eda29be0dfc4c4127fb1c80077b7",
-        #     port="5432"
-        # )
 
         cursor = connection.cursor()
+        cursor.execute("SELECT * FROM test WHERE test_id = 1")
 
-        return cursor.fetchall(
-            "SELECT * FROM test WHERE test_id = 1"
-        )
+        return cursor.fetchall()
 
-    except (ConnectionError, ConnectionRefusedError) as e:
+    except psycopg2.Error as e:
         return {"message": f"error occurred: {e}"}
-
