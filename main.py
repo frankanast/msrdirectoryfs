d1bbd9956e4ed96a14ddeff1c24712aa514aa972
franc <francesco.anastasio93@gmail.com>
2024-03-19 18:47:04 +0100

    Restored to real use case.

--- main.py
+++ main.py
@@ -19,13 +19,4 @@ async def hworld(name: str):
 
 @app.get("/autocomplete_data")
 async def get_autocomplete_data():
-    try:
-        connection = psycopg2.connect(DATABASE_URL, sslmode='require')
-
-        cursor = connection.cursor()
-        cursor.execute("SELECT * FROM test WHERE test_id = 1")
-
-        return cursor.fetchall()
-
-    except psycopg2.Error as e:
-        return {"message": f"error occurred: {e}"}
+    return DATABASE_URL
