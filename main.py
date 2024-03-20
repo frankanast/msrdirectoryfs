80128c5d03d364a500b3b6597137917cfb7bf2f8
franc <francesco.anastasio93@gmail.com>
2024-03-20 21:25:46 +0100

    Restored to real use case.

--- main.py
+++ main.py
@@ -32,10 +32,40 @@ async def get_autocomplete_data():
 
 
 @app.get("/import_backup")
-async def import_csv(filepath: str = 'backup.csv') -> bool:
+async def import_csv(filepath: str = 'backup.csv'):
     # For internal use only (for now). Imports a CSV directory in the Heroku database.
     # Servizio; Società; Riferimento; Recapito; Altri recapiti; Email; Indirizzo; Paese; Note
-    return False
+    import csv  # We only work with CSS in this place.
+
+    try:
+        connection = psycopg2.connect(DATABASE_URL)
+        cursor = connection.cursor()
+    except (ConnectionError, ConnectionRefusedError, ConnectionAbortedError) as conn_e:
+        print('A connection error occurred: ', conn_e)
+        return False
+
+    try:
+        with open(filepath, 'r', newline='') as csvfile:
+            csv_reader = csv.reader(csvfile)
+            str_ = str()  # debug
+
+            for row in csv_reader:
+                str_ += str(row)
+
+            return str_
+
+    except FileNotFoundError:
+        print("File not found.")
+        return False
+    except Exception as e:
+        print("An error occurred:", e)
+        return False
+
+    cursor.execute("SELECT name FROM categories")
+    data = [row[0] for row in cursor.fetchall()]
+
+    cursor.close()
+    connection.close()
 
 if __name__ == '__main__':
     import_csv('')
